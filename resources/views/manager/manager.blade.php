bcb2889a43804430772416561a9436c7ef641f05
GorDi-Ipzk231 <144680469+GorDi-Ipzk231@users.noreply.github.com>
2024-06-10 00:54:26 +0300

    add stylisation to manager table

--- resources/views/manager/manager.blade.php
+++ resources/views/manager/manager.blade.php
@@ -11,33 +11,34 @@
     <a type="button" class=" m-1 btn btn-warning mb-3" style="width: 150px;"
         href="{{route('manager.create') }}">Create</a>
 
-    <table class="table m-3 border">
+    <table class="table table-bordered m-3">
 
-
-        <thead>
+        <thead class="thead-dark">
             <th>First Name</th>
             <th>Last Name</th>
             <th>Contact Phone</th>
             <th>Contact Email</th>
-            <th style="{{ auth()->user()->role->name == 'Customer' ? 'visibility:hidden' : 'visibility:visible' }}">Actions</th>
+            <th style="{{ auth()->user()->role->name == 'Customer' ? 'visibility:hidden' : 'visibility:visible' }}">
+                Actions</th>
         </thead>
 
         @foreach($managers as $manager)
-        <tr>
-            <td>{{$manager->first_name}}</td>
-            <td>{{$manager->last_name}}</td>
-            <td>{{$manager->contact_phone}}</td>
-            <td>{{$manager->contact_email}}</td>
-            <td class="d-flex" style="{{ auth()->user()->role->name == 'Customer' ? 'visibility:hidden' : 'visibility:visible' }}">
-                <a type="button" class=" m-1 btn btn-info" href="{{route('manager.edit', $manager->id) }}">Edit</a>
-                <a type="button" class=" m-1 btn btn-dark" href="{{route('manager.show', $manager->id) }}">Show</a>
-                <form action="{{route('manager.destroy', $manager->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" class=" m-1 btn btn-danger" value="Delete">
-                </form>
-            </td>
-        </tr>
+            <tr>
+                <td>{{$manager->first_name}}</td>
+                <td>{{$manager->last_name}}</td>
+                <td>{{$manager->contact_phone}}</td>
+                <td>{{$manager->contact_email}}</td>
+                <td class="d-flex"
+                    style="{{ auth()->user()->role->name == 'Customer' ? 'visibility:hidden' : 'visibility:visible' }}">
+                    <a type="button" class=" m-1 btn btn-info" href="{{route('manager.edit', $manager->id) }}">Edit</a>
+                    <a type="button" class=" m-1 btn btn-dark" href="{{route('manager.show', $manager->id) }}">Show</a>
+                    <form action="{{route('manager.destroy', $manager->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" class=" m-1 btn btn-danger" value="Delete">
+                    </form>
+                </td>
+            </tr>
         @endforeach
 
 
